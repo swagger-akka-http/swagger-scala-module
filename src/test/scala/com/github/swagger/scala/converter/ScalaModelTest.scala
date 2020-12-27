cb4d8866c15ead0140aa6c954177af3b6b897593
PJ Fanning <pjfanning@users.noreply.github.com>
2020-12-27 18:57:06 +0000

    Update ScalaModelTest.scala

--- src/test/scala/com/github/swagger/scala/converter/ScalaModelTest.scala
+++ src/test/scala/com/github/swagger/scala/converter/ScalaModelTest.scala
@@ -40,10 +40,10 @@ class ScalaModelTest extends AnyFlatSpec with Matchers {
     val schemas = ModelConverters.getInstance().readAll(classOf[ModelWithTestEnum]).asScala
     val userSchema = schemas("ModelWithTestEnum")
 
-    val orderSize = userSchema.getProperties().get("enum")
-    orderSize should not be null
-    orderSize shouldBe a [StringSchema]
-    val sp = orderSize.asInstanceOf[StringSchema]
+    val enum = userSchema.getProperties().get("enum")
+    enum should not be null
+    enum shouldBe a [StringSchema]
+    val sp = enum.asInstanceOf[StringSchema]
     Option(sp.getEnum) shouldBe defined
     sp.getEnum().asScala.toSet shouldEqual Set("a", "b")
   }
