cd321018e210f3273662f2865141d2a6e121832b
Sam Theisens <1911436+SamTheisens@users.noreply.github.com>
2022-08-07 01:36:29 +0800

    Retrieve fields via apply method when possible (#169)
    which is the only option if the case class is defined nested within
    an object.

--- src/main/scala-2/com/github/swagger/scala/converter/ErasureHelper.scala
+++ src/main/scala-2/com/github/swagger/scala/converter/ErasureHelper.scala
@@ -1,16 +1,26 @@
 package com.github.swagger.scala.converter
 
+import scala.reflect.runtime.universe.TermName
+
 object ErasureHelper {
 
   def erasedOptionalPrimitives(cls: Class[_]): Map[String, Class[_]] = {
     import scala.reflect.runtime.universe
     val mirror = universe.runtimeMirror(cls.getClassLoader)
-    val sym = mirror.staticClass(cls.getName)
-    val properties = sym.selfType.members
-      .filterNot(_.isMethod)
-      .filterNot(_.isClass)
 
-    properties.flatMap { prop =>
+    val moduleSymbol = mirror.moduleSymbol(Class.forName(cls.getName))
+    val ConstructorName = "apply"
+    val companion: universe.Symbol = moduleSymbol.typeSignature.member(TermName(ConstructorName))
+    val properties = if (companion.fullName.endsWith(ConstructorName)) {
+      companion.asMethod.paramLists.flatten
+    } else {
+      val sym = mirror.staticClass(cls.getName)
+      sym.selfType.members
+        .filterNot(_.isMethod)
+        .filterNot(_.isClass)
+    }
+
+    properties.flatMap { prop: universe.Symbol =>
       val maybeClass: Option[Class[_]] = prop.typeSignature.typeArgs.headOption.flatMap { signature =>
         if (signature.typeSymbol.isClass) {
           Option(mirror.runtimeClass(signature.typeSymbol.asClass))
--- src/test/scala/models/ModelWOptionInt.scala
+++ src/test/scala/models/ModelWOptionInt.scala
@@ -4,6 +4,11 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 case class ModelWOptionInt(optInt: Option[Int])
 
+object NestingObject {
+  case class NestedModelWOptionInt(optInt: Option[Int])
+  case class NestedModelWOptionIntSchemaOverride(@Schema(description = "This is an optional int") optInt: Option[Int])
+}
+
 case class ModelWOptionIntSchemaOverride(@Schema(description = "This is an optional int") optInt: Option[Int])
 
 case class ModelWOptionIntSchemaOverrideForRequired(@Schema(required = true) optInt: Option[Int])
