5bac23ff475c80d1c061bfdac4e50523ec46796b
PJ Fanning <pjfanning@users.noreply.github.com>
2022-08-07 16:10:42 +0100

    Update ErasureHelper.scala

--- src/main/scala-2/com/github/swagger/scala/converter/ErasureHelper.scala
+++ src/main/scala-2/com/github/swagger/scala/converter/ErasureHelper.scala
@@ -34,7 +34,8 @@ private[converter] object ErasureHelper {
     } catch {
       case NonFatal(t) => {
         if (logger.isDebugEnabled) {
-          logger.debug("Unable to get type info {}", Option(cls.getName).getOrElse("null"), t)
+          //use this form because of Scala 2.11 & 2.12 compile issue
+          logger.debug(s"Unable to get type info ${Option(cls.getName).getOrElse("null")}", t)
         } else {
           logger.info("Unable to get type info {}", Option(cls.getName).getOrElse("null"))
         }
