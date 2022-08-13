e72000d0c0d47db54851d40a53357ab399ba185f
Sam Theisens <1911436+SamTheisens@users.noreply.github.com>
2022-08-13 17:41:44 +0800

    Improve maintainability of test suite (#178)
    * Pull repeated converter instantiation up into trait
    
    branch:
    
    * Add .scalafmt for consistent formatting
    
    trying to match the existing formatting as closely as possible
    https://scalameta.org/scalafmt/
    
    branch:
    
    * Pull repeated schema logic up into scope
    
    branch:
    
    * Add debug flag to test cases
    
    that dumps the schema as JSON. This shows what's happening in one
    overview, which can be helpful when debugging
    
    branch:

--- src/test/scala/models/CoreBug814.scala
+++ src/test/scala/models/CoreBug814.scala
@@ -4,6 +4,6 @@ import io.swagger.v3.oas.annotations.Operation
 
 import scala.annotation.meta.field
 
-case class CoreBug814 (
-  @(Operation @field)(description="true if open to the public") isFoo: Boolean
+case class CoreBug814(
+    @(Operation @field)(description = "true if open to the public") isFoo: Boolean
 )
--- src/test/scala/models/ModelWSeqInt.scala
+++ src/test/scala/models/ModelWSeqInt.scala
@@ -6,4 +6,6 @@ case class ModelWSeqInt(ints: Seq[Int])
 
 case class ModelWSeqIntDefaulted(ints: Seq[Int] = Seq.empty)
 
-case class ModelWSeqIntAnnotated(@ArraySchema(arraySchema = new Schema(required = false), schema = new Schema(description = "These are ints")) ints: Seq[Int])
+case class ModelWSeqIntAnnotated(
+    @ArraySchema(arraySchema = new Schema(required = false), schema = new Schema(description = "These are ints")) ints: Seq[Int]
+)
