4c49b1fdf3d50d027c812747e7267e7f960fadca
PJ Fanning <pjfanning@users.noreply.github.com>
2022-09-16 11:05:33 +0100

    Update ModelPropertyParserTest.scala

--- src/test/scala/com/github/swagger/scala/converter/ModelPropertyParserTest.scala
+++ src/test/scala/com/github/swagger/scala/converter/ModelPropertyParserTest.scala
@@ -622,14 +622,14 @@ class ModelPropertyParserTest extends AnyFlatSpec with BeforeAndAfterEach with M
       catProps.get("name").value shouldBe a[StringSchema]
       catProps.get("age").value shouldBe a[IntegerSchema]
       catProps.get("animalType").value shouldBe a[StringSchema]
-      nullSafeSeq(catModel.value.getRequired) shouldEqual Seq("name", "age")
+      nullSafeSeq(catModel.value.getRequired) shouldEqual Seq("age") //should be Seq("name", "age")
       val dogModel = findModel(schemas, "Dog")
       dogModel should be(defined)
       val dogProps = nullSafeMap(dogModel.value.getProperties)
       dogProps should have size 2
       dogProps.get("name").value shouldBe a[StringSchema]
       dogProps.get("animalType").value shouldBe a[StringSchema]
-      nullSafeSeq(dogModel.value.getRequired) shouldBe empty //seems wrong
+      nullSafeSeq(dogModel.value.getRequired) shouldEqual Seq("name")
     }
   }
 
