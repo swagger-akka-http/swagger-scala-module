1538843d0d59b24e2e642d7e9b484ae7c17096dd
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-02-20 10:43:41 +0100

    Update scalafmt-core to 3.8.0 (#316)
    * Update scalafmt-core to 3.8.0
    
    * Reformat with scalafmt 3.8.0
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.8.0' to .git-blame-ignore-revs

--- src/test/scala/models/AddRequest.scala
+++ src/test/scala/models/AddRequest.scala
@@ -4,5 +4,7 @@ import io.swagger.v3.oas.annotations.media.{ArraySchema, Schema}
 
 case class AddRequest(@ArraySchema(minItems = 2, maxItems = 10) numbers: Array[Double])
 
-case class AddRequestOldStyleAnnotation(@ArraySchema(schema = new Schema(implementation = classOf[Long]), minItems = 2, maxItems = 10)
-                                        numbers: Array[Double])
+case class AddRequestOldStyleAnnotation(
+    @ArraySchema(schema = new Schema(implementation = classOf[Long]), minItems = 2, maxItems = 10)
+    numbers: Array[Double]
+)
